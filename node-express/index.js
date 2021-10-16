d53b4d681fbc1bbb8702fea551cd9e8f0a2b9a22
John Lenon Nascimento da Silva <john.silva@capgemini.com>
2021-10-16 09:04:46 -0300

    Coursera - Creating the first entry of an express server (REST API)

--- node-express/index.js
+++ node-express/index.js
@@ -1,14 +1,25 @@
-const express = require(`express`),
-  http = require(`http`),
-  morgan = require(`morgan`),
-  hostname = `localhost`,
+const express = require('express'),
+  http = require('http'),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  hostname = 'localhost',
   port = 3000,
   app = express();
 
-app.use(morgan(`dev`));
-
-app.use(express.static(__dirname + `/public`));
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-  console.log(`req.headers is: ` + req.headers);
+  console.log('req.headers is: ' + req.headers);
+  const stringifiedData = JSON.stringify(req.headers);
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an express server ' + req.body + '</h1></body></html>');
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
