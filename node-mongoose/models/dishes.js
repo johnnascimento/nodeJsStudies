555b5d645b29e55bdd673b18d81064231bec0151
Silva <john.silva@capgemini.com>
2021-12-22 15:05:53 -0300

    Mongoose part 3

--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+var commentSchema = new Schema(
+    {
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5,
+            required: true
+        },
+
+        comment: {
+            type: String,
+            required: true
+        },
+
+        author: {
+            type: String,
+            required: true
+        },
+    },
+    {
+        timespans: true
+    }
+);
+
 const dishSchema = new Schema(
     {
         name: {
@@ -11,7 +35,8 @@ const dishSchema = new Schema(
         description: {
             type: String,
             required: true
-        }
+        },
+        comments: [commentSchema]
     },
     {
         timestamps: true
