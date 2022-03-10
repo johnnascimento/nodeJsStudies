be90e26e8f4a13c499687467c61f7be692e1544a
Silva <john.silva@capgemini.com>
2022-03-10 10:51:15 -0300

    Uploading image lesson

--- conFusionServer/controllers/uploadImage.js
+++ conFusionServer/controllers/uploadImage.js
@@ -1,42 +1,10 @@
 const fs = require('fs'),
-    mongoose = require('mongoose'),
-    multer = require('multer');
-
-    storage = multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, 'public/image');
-        },
-
-        filename: (req, file, cb) => {
-            cb(null, file.originalname);
-        }
-    }),
-
-    imageFileFilter = (req, file, cb) => {
-        if (!file.originalname.match(/|.(jpg|jpeg|png|gif)$/)) {
-            let notAllowedFileError = new Error('You can upload only image files!');
-
-            return cb(notAllowedFileError, false);
-        }
-
-        cb(null, true);
-    },
-
-    upload = multer({
-        storage: storage,
-        fileFilter: imageFileFilter
-    });
+    mongoose = require('mongoose');
 
 module.exports = {
     getImage: (req, res, next) => {
         res.statusCode = 403;
         res.end('GET operation not supported on /imageUpload');
-    },
-
-    postImage: (req, res, next) => {
-        console.log('postImage', req.file);
-
-        upload.single('imageFile');
     },
 
     putImage: (req, res, next) => {
@@ -55,7 +23,14 @@ module.exports = {
 
         console.log('handleUploadedImage', res.statusCode);
         console.log('handleUploadedImage', res.getHeader('Content-Type'));
+
+        console.log('***********************************');
+        console.log('***********************************');
+        console.log('***********************************');
         console.log('handleUploadedImage', req.file);
+        console.log('***********************************');
+        console.log('***********************************');
+        console.log('***********************************');
 
         res.json(req.file);
     }
--- conFusionServer/routes/uploadRouter.js
+++ conFusionServer/routes/uploadRouter.js
@@ -1,7 +1,39 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
     authenticate = require('../authenticate'),
-    uploadRouter = express.Router();
+    uploadRouter = express.Router(),
+    multer = require('multer'),
+
+    storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            console.log('destination', file);
+
+            cb(null, 'public/images');
+        },
+
+        filename: (req, file, cb) => {
+            console.log('filename', file);
+
+            cb(null, file.originalname);
+        }
+    }),
+
+    imageFileFilter = (req, file, cb) => {
+        console.log('imageFileFilter', file);
+
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+            let notAllowedFileError = new Error('You can upload only image files!');
+
+            return cb(notAllowedFileError, false);
+        }
+
+        cb(null, true);
+    },
+
+    upload = multer({
+        storage: storage,
+        fileFilter: imageFileFilter
+    });
 
 // Importing controllers
 var ctrlUploadImage = require('../controllers/uploadImage');
@@ -12,7 +44,7 @@ uploadRouter.use(bodyParser.json());
 /* GET Upload listing. */
 uploadRouter.route('/')
     .get(authenticate.verifyUser, authenticate.verifyAdmin, ctrlUploadImage.getImage)
-    .post(authenticate.verifyUser, authenticate.verifyAdmin, ctrlUploadImage.postImage, ctrlUploadImage.handleUploadedImage)
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), ctrlUploadImage.handleUploadedImage)
     .put(authenticate.verifyUser, authenticate.verifyAdmin, ctrlUploadImage.putImage)
     .delete(authenticate.verifyUser, authenticate.verifyAdmin, ctrlUploadImage.deleteImage);
 
