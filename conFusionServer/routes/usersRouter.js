79c70f33b3ed16718d2d7076aa95f70c37c56274
Silva <john.silva@capgemini.com>
2022-04-14 10:06:56 -0300

    Updating facebook's index button

--- conFusionServer/routes/usersRouter.js
+++ conFusionServer/routes/usersRouter.js
@@ -19,7 +19,7 @@ userRouter
     .get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, ctrlUsers.getUsers)
     .post('/signup', cors.corsWithOptions, ctrlUsers.signUp)
     .post('/login', cors.corsWithOptions, passport.authenticate('local'), ctrlUsers.logIn)
-    .get('/logout', cors.corsWithOptions, ctrlUsers.logOut);
+    .get('/logout', cors.corsWithOptions, ctrlUsers.logOut)
     .get('/facebook/token', passport.authenticate('facebook-token'), ctrlUsers.facebookOAuth);
 
 module.exports = userRouter;
